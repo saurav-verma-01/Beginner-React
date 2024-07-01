31284bf6d47d084ab2ecccef9dd51bfcb05e9ecc
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-07-01 15:54:30 +0530

    vite app test

---
+++ test-vite/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1>React App with Vite.</h1>
+      <h2>Faster way to create React Applications.</h2>
+      <h3>Hello World</h3>
+    </div>
+  );
+};
+
+export default App;
