889271826950b311257b1da1285b4d65d35f1210
saurav-verma-01 <sauravkumarverma22@gmail.com>
2024-07-01 22:09:21 +0530

    user card project completion

--- user-card/src/App.jsx
+++ user-card/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from "./Card";
+import { CardClass } from "./CardClass";
+import user from "./user.json";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const { name, age, address, phoneNumber } = user;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Card name={name} age={age} address={address} phoneNumber={phoneNumber} />
+      <Card
+        name="Saurav Verma"
+        age={26}
+        address="58 Nandanvan, Vrindavan"
+        phoneNumber="7351-488-554"
+      />
+      <CardClass
+        name="Tushar Gautam "
+        age={26}
+        address="Ganeshara, Mathura"
+        phoneNumber="7351-488-554"
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ user-card/src/Card.jsx
@@ -0,0 +1,17 @@
+const Card = () => {
+  return (
+    <div className="card">
+      <h2 className="name">Kyle Cook</h2>
+      <div className="body">
+        <div className="label">Age:</div>
+        <div>27</div>
+        <div className="label">Phone:</div>
+        <div>123-456-7890</div>
+        <div className="label">Address:</div>
+        <div>123 Main St.</div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
